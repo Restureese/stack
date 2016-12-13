59443f4fc104875d81617023f1daf9b3ecad1c53
restureese <restuankanser@gmail.com>
2016-12-14 01:32:19 +0700

    implementasi stack

--- implementasi stack membaca buku.cpp
+++ implementasi stack membaca buku.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <cstdlib>
-
+#define MAX 5
 using namespace std;
 
-const int MAX = 5;
 
 typedef int element;
 struct StackRecord{
 	int top;
-	element StackInfo[MAX];
+	string StackInfo[MAX];
 	element StackArray[MAX]; //array stack
 };
 
@@ -17,25 +15,26 @@ typedef StackRecord Stack;
 void initStack(Stack &S);
 bool isEmpty(Stack S);
 bool isFull(Stack S);
-void push(Stack &S,element value,element info);
+void push(Stack &S,element value,string info);
 element pop(Stack &S);
-void printStack(Stack S);
 void ListBuku();
 void hitungWaktu(Stack S);
 void Waktu(Stack S,int total);
 
-int perHal;
+int perHal=0;
 
 int main(){
-	element hal;
+
 	int book;
 	int menit,jam,totWaktu;
 	int pilih;
 	int jmlBuku;
-	string ulang;
+	string ulang,namaBuku;
 	Stack buku;
 	initStack(buku);
 
+	cout<<"Masukkan waktu rata - rata membaca per halaman : ";
+	cin>>perHal;
 	do{
 		ulang:
 		cout<<"==========================================================================="<<endl;
@@ -57,20 +56,29 @@ int main(){
 					ListBuku();
 					cout<<"Pilih Buku Yang Akan Dipinjam : ";
 					cin>>book;
-					if(book==1)
-						push(buku,1127,1);
-					else if(book==2)
-						push(buku,1220,2);
-					else if(book==3)
-						push(buku,567,3);
-					else if(book==4)
-						push(buku,982,4);
-					else if(book==5)
-						push(buku,748,5);
+					if(book==1){
+						namaBuku = "Algortima Pemrograman";
+						push(buku,1127,namaBuku);
+					}
+					else if(book==2){
+						namaBuku = "Make Your Application With Python";
+						push(buku,1220,namaBuku);
+					}
+					else if(book==3){
+						namaBuku = "Tutorial Membuat Web Dengan Django";
+						push(buku,567,namaBuku);
+					}
+					else if(book==4){
+						namaBuku = "Object Oriented Programming";
+						push(buku,982,namaBuku);
+					}
+					else if(book==5){
+						namaBuku = "Linux Administation";
+						push(buku,748,namaBuku);
+					}
 					else
 					 	cout<<"Buku Yang Anda Pilih Tidak Tersedia"<<endl;
 				}
-				system("clear");
 				goto ulang;
 				break;
 			case 2:
@@ -80,14 +88,10 @@ int main(){
 				cin>>menit;
 				totWaktu = jam * 60 + menit;
 				Waktu(buku,totWaktu);
-				system("clear");
 				goto ulang;
 				break;
 			case 3:
-				cout<<"Masukkan waktu rata - rata membaca per halaman : ";
-				cin>>perHal;
 				hitungWaktu(buku);
-				system("clear");
 				goto ulang;
 				break;
 			case 0:
@@ -113,9 +117,9 @@ bool isFull(Stack S){
 	return S.top == MAX - 1;
 }
 
-void push(Stack &S,element value,element info){
+void push(Stack &S,element value,string info){
 	if(isFull(S)){
-		cout<<"Stack Penuh !"<<endl;
+		cout<<"Banyak Peminjaman Sudah Penuh !"<<endl;
 	}else{
 		S.top++;
 		S.StackInfo[S.top] = info;
@@ -126,7 +130,7 @@ void push(Stack &S,element value,element info){
 element pop(Stack &S){
 	element temp;
 	if(isEmpty(S)){
-		cout<<"Stack Masih Kosong !"<<endl;
+		cout<<"Anda Belum Pinjam !"<<endl;
 	}else{
 		temp = S.StackArray[S.top];
 		S.top--;
@@ -134,22 +138,9 @@ element pop(Stack &S){
 	return temp;
 }
 
-void printStack(Stack S){
-	element value;
-	if(isEmpty(S)){
-		cout<<"Stack Masih Kosong!"<<endl;
-	}else{
-		while(S.top!=-1){
-			value = pop(S);
-			cout<<"||"<<value<<"||"<<endl;
-		}
-		cout<<"===="<<endl;
-	}
-}
-
 void ListBuku(){
 	cout<<"1. Algortima Pemrograman 1127 Halaman"<<endl;
-	cout<<"2. Make Your Aplication With Pyhon 1220 Halaman"<<endl;
+	cout<<"2. Make Your Application With Python 1220 Halaman"<<endl;
 	cout<<"3. Tutorial Membuat Web Dengan Django 567 Halaman"<<endl;
 	cout<<"4. Object Oriented Programming 982 Halaman"<<endl;
 	cout<<"5. Linux Administation 748 Halaman"<<endl;
@@ -161,8 +152,8 @@ void hitungWaktu(Stack S){
 	int menit = 0; //menit
 	while(S.top != -1){
 		total += pop(S);
-
 	}
+
 	total*=perHal;
 
 	jam = total / 60;
@@ -172,7 +163,7 @@ void hitungWaktu(Stack S){
 
 void Waktu(Stack S,int total){
 	if(total< S.StackArray[S.top]*perHal){
-		cout<<"Masih Membaca Buku No-"<<S.StackInfo[S.top]<<endl;
+		cout<<"Masih Membaca Buku "<<S.StackInfo[S.top]<<endl;
 		cout<<"Tersisa "<<S.StackArray[S.top] - (total/perHal)<<" Halaman"<<endl;
 	}else{
 		total -= S.StackArray[S.top]*perHal;
